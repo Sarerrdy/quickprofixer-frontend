2da883c0703df8c4aa6981568cf848f380cf80a8
Edmond Ina <sarerrdy4live@live.co.uk>
2025-07-02 01:12:53 +0200

    updated footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto px-4 text-center">
-        <p>Contact Information | Social Media Links</p>
+      <div className="container mx-auto px-4 text-center">       
         <p>&copy; 2025 QuickProFixer. All rights reserved.</p>
       </div>
     </footer>
